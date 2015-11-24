bbab312a677cc496f1b0ce71dbe80432a59f13ad
Kamil Kulak <kulak.kam@gmail.com>
2015-11-24 09:47:30 +0100

    Uncomment

--- src/main/scala/pl/edu/agh/ecommerce/Application.scala
+++ src/main/scala/pl/edu/agh/ecommerce/Application.scala
@@ -7,10 +7,10 @@ import pl.edu.agh.ecommerce.Initializer.Initialize
 object Application extends App {
   implicit val config = ConfigFactory.load()
   val auctionSystem = ActorSystem("auction-system", config.getConfig("auction-system").withFallback(config))
-  //val auctionPublisher = ActorSystem("auction-publisher", config.getConfig("auction-publisher").withFallback(config))
+  val auctionPublisher = ActorSystem("auction-publisher", config.getConfig("auction-publisher").withFallback(config))
 
   auctionSystem.actorOf(AuctionSearch.props(), "auction-search")
-  //auctionPublisher.actorOf(Publisher.props(), "publisher")
+  auctionPublisher.actorOf(Publisher.props(), "publisher")
 
   val initializer = auctionSystem.actorOf(Initializer.props())
   initializer ! Initialize
